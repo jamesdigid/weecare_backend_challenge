2ae9f4a8d04a22177fcfc4367792a3f008294a5f
James Jones <james.jones@nepservices.com>
2019-05-13 18:09:16 -0700

    Creating GET albums api endpoint

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,37 @@ use Illuminate\Http\Request;
 |
 */
 
+// Public
+Route::group(['prefix'     => 'v1',], function() {
+    Route::post('register', 'UserController@register')->name('api.register');
+    Route::post('login', 'UserController@authenticate')->name('api.authenticate');
+    Route::apiResource('albums', 'AlbumController');
+    
+    //Route::get('albums', 'AlbumController@getAlbums')->name('api.albums');
+    //Route::get('albums/{id}', 'AlbumController@getAlbumIndex');
+
+
+});
+
+// Protected
+Route::group([
+    'middleware' => ['jwt.verify'],  
+    'prefix'     => 'v1',], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+});
+
+    /*
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// User //
+Route::post('authenticate', 'AuthController@authenticate')->name('api.authenticate');
+Route::post('register', 'AuthController@register')->name('api.register');
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    // Albums //
+    Route::get('albums', 'AlbumController@getAlbums')->name('api.albums');
+});*/
